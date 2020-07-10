009605fbff7c9c125922a5bc025dcb0e55019dbe
OrkunSA <c.orkun.s@gmail.com>
2020-07-09 22:39:47 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,22 @@
-class PigLatinizer 
-    def initialize 
-    end
+class PigLatinizer
 
-    def piglatinize(words)
-        word_array = words.split(" ")
-        return_string = ""
-        word_array.each do |word|
-            if word.match(/\A+[AEIOU]/i) != nil
-                return_string << word.strip + "way" + " "
-            else
-                consonants = word.scan(/\A+[^AEIOU]+/i)
-                word = word.sub(consonants.join, '') + consonants.join + 'ay'
-                return_string << word.strip+" "
-            end
-        end
-        return_string.strip 
+  def initialize
+
+  end
+
+  def piglatinize(words)
+    word_array = words.split(" ")
+    return_string = ""
+    word_array.each do |word|
+      if word.match(/\A+[AEIOU]/i) != nil
+        return_string << word.strip + "way" + " "
+      else
+        
+        consonants = word.scan(/\A+[^AEIOU]+/i)
+        word = word.sub(consonants.join, '') + consonants.join + 'ay'
+        return_string << word.strip+" "
+      end
     end
+    return_string.strip
+  end
 end
